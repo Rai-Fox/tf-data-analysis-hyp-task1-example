22da474e1a67d611cb660f4c91509b32ecd3634e
raifox <raifox@yandex-team.ru>
2023-04-10 19:06:20 +0300

    solution

--- solution.py
+++ solution.py
@@ -1,14 +1,16 @@
 import pandas as pd
 import numpy as np
 
+from scipy.stats import binom
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 222287279 # Ваш chat ID, не меняйте название переменной
 
-def solution(x_success: int, 
-             x_cnt: int, 
-             y_success: int, 
-             y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(
+        x_success: int, 
+        x_cnt: int, 
+        y_success: int, 
+        y_cnt: int,
+) -> bool:
+    alpha = 0.03
+    p_value = binom(n=y_cnt, p=x_success/x_cnt).cdf(y_success)
+    return p_value > 1 - alpha
